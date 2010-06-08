1ea08dda9f8e46f71b8272326c6dc47d423df1a2
Andrew Butcher <abutcher@afrolegs.com>
2010-06-08 14:51:47 -0400

    Adding base costs to new models and some code to the arff translator

--- src/python/arff.py
+++ src/python/arff.py
@@ -1,9 +1,25 @@
 import re, sys
 
+name = ""
+attributes = []
+data = []
+
 def arfftoarr(pathtoarff):
     arff = open(pathtoarff,'r')
-    data = [][]
 
     for line in arff:
-        if re.match(REGULAREXPRESSION, line):
-            
+        line = line.replace("\n","")
+        if re.match("^@relation", line):
+            name = line.split(" ")[1]
+        elif re.match("^@attribute", line):
+            attributes.append(line.split(" ")[1])
+        elif re.match("^@data", line):
+            pass
+        elif line != "":
+            data.append(line.split(","))
+
+    print name
+    print attributes
+    print data
+
+arfftoarr("arff/telecom1.arff")            
