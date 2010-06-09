de52be1f44ba0aaeccf06ebb65a3e2bc062f0ac9
Andrew Butcher <abutcher@afrolegs.com>
2010-06-09 14:54:34 -0400

    Adding some code to handle complex numbers in the pruner.

--- src/lisp/mre.lisp
+++ src/lisp/mre.lisp
@@ -1,5 +1,7 @@
 (defun mre (actual predicted)
   (if (= actual predicted)
       0
-      (/ (abs (- actual predicted))
-	 actual)))
+      (if (= actual 0)
+	  (setf actual .00000000001)
+	  (/ (abs (- actual predicted))
+	     actual))))
