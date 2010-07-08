04305b957d671645b37a131ec7bc77a6b40de45a
Andrew Butcher <abutcher@afrolegs.com>
2010-07-08 00:09:22 -0400

    Added a gynsym for making danglers unique and fixed all the images.

--- src/lisp/dot.lisp
+++ src/lisp/dot.lisp
@@ -6,23 +6,43 @@
     (format stream "DIGRAPH G {~%")
     (labels ((walk (c-node &optional (level 0))
 	       (unless (null (node-right c-node))
-		 (format stream "\"~A\" -> \"~A\" L=~A;~%"
-			 (format nil "Size: ~A\\nVariance: ~A"
-				 (length (node-contents c-node))
-				 (node-variance c-node))
-			 (format nil "Size: ~A\\nVariance: ~A"
-				 (length (node-contents (node-right c-node)))
-				 (node-variance (node-right c-node)))
-			 level))
+		 (if (= (length (node-contents (node-right c-node))) 1)
+		     (format stream "\"~A\" -> \"~A\" L=~A;~%"
+			     (format nil "Size: ~A\\nVariance: ~A"
+				     (length (node-contents c-node))
+				     (node-variance c-node))
+			     (format nil "Size: ~A\\nVariance: ~A\\n~A"
+				     (length (node-contents (node-right c-node)))
+				     (node-variance (node-right c-node))
+				     (gensym))
+			     level)
+		     (format stream "\"~A\" -> \"~A\" L=~A;~%"
+			     (format nil "Size: ~A\\nVariance: ~A"
+				     (length (node-contents c-node))
+				     (node-variance c-node))
+			     (format nil "Size: ~A\\nVariance: ~A"
+				     (length (node-contents (node-right c-node)))
+				     (node-variance (node-right c-node)))
+			     level)))
 	       (unless (null (node-left c-node))
-		 (format stream "\"~A\" -> \"~A\" L=~A;~%"
-			 (format nil "Size: ~A\\nVariance: ~A"
-				 (length (node-contents c-node))
-				 (node-variance c-node))
-			 (format nil "Size: ~A\\nVariance: ~A"
-				 (length (node-contents (node-left c-node)))
-				 (node-variance (node-left c-node)))
-			 level))
+		 (if (= (length (node-contents (node-left c-node))) 1)
+		     (format stream "\"~A\" -> \"~A\" L=~A;~%"
+			     (format nil "Size: ~A\\nVariance: ~A"
+				     (length (node-contents c-node))
+				     (node-variance c-node))
+			     (format nil "Size: ~A\\nVariance: ~A\\n~A"
+				     (length (node-contents (node-left c-node)))
+				     (node-variance (node-left c-node))
+				     (gensym))
+			     level)
+		     (format stream "\"~A\" -> \"~A\" L=~A;~%"
+			     (format nil "Size: ~A\\nVariance: ~A"
+				     (length (node-contents c-node))
+				     (node-variance c-node))
+			     (format nil "Size: ~A\\nVariance: ~A"
+				     (length (node-contents (node-left c-node)))
+				     (node-variance (node-left c-node)))
+			     level)))
 	       (unless (null (node-right c-node))	     
 		 (walk (node-right c-node) (1+ level)))
 	       (unless (null (node-left c-node))
