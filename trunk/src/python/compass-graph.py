87aa462e0b20f1a258d9b2a78ed1277cc7b3828e
Kel Cecil <kelcecil@praisechaos.com>
2010-09-29 00:50:14 -0400

    fixed graph to use matplotlib

--- trunk/src/python/compass-graph.py
+++ trunk/src/python/compass-graph.py
@@ -3,6 +3,7 @@
 from arff import *
 import csv
 import numpy
+import matplotlib.pyplot as plt
 from optparse import OptionParser
 from util import *
 
@@ -10,35 +11,59 @@ class CompassGraph:
 	East = None
 	West = None
 	data = []
-	DataCoordinates = numpy.array([])
+	DataCoordinates = []
+	Classes = []
 
 	def __init__(self, InputData):
 		self.data = InputData
-		(self.East, self.West) = FindPoles(InputData)
-		self.DataCoordinates = ComputeCoordinates(data, East, West)
+		(self.East, self.West) = self.FindPoles(InputData)
+		(self.DataCoordinates, self.Classes) = self.ComputeCoordinates()
 
-	def ComputeCoordinates(data, East, West):
-		d = distance(East,West)
+	def WriteToPNG(self,filename):
+		self.DataCoordinates = self.DataCoordinates.transpose()
+		plt.axis([0,1,0,1])
+		plt.title(filename)
+		plt.xlabel("x")
+		plt.ylabel("y")
+		plt.grid(True)
+		if isinstance(self.Classes[0][0],str):
+			for i in range(len(self.DataCoordinates[0])):
+				if self.Classes[i][0].upper() == "TRUE":
+					plt.plot(self.DataCoordinates[0][i],self.DataCoordinates[1][i],"bo")
+				else:
+					plt.plot(self.DataCoordinates[0][i],self.DataCoordinates[1][i],"ro")
+		a = plt.gca()
+		plt.show()
+				
+
+	def ComputeCoordinates(self):
+		d = distance(self.East,self.West)
 		MaxX = 0
 		MaxY = 0
+		DataCoordinates = None
 		# Compute (x,y) coordinates for each instance
-		for instance in data:
-			a = distance(East,instance)
-			b = distance(West,instance)
+		for instance in self.data:
+			a = distance(self.East,instance)
+			b = distance(self.West,instance)
 			x = (b**2 - d**2 - a**2) / (-2 * d)
 			if x > MaxX:
 				MaxX = x
 			y = math.sqrt(a**2 - x**2)
 			if y > MaxY:
 				MaxY = y
-			self.DataCoordinates = numpy.vstack((self.DataCoordinates,numpy.array([x,y])))
+			if DataCoordinates is None:
+				DataCoordinates = numpy.array([x,y])
+				Classes = numpy.array([instance[len(instance)-1]])
+			else:
+				DataCoordinates = numpy.vstack((DataCoordinates,numpy.array([x,y])))
+				Classes = numpy.vstack((Classes,numpy.array([instance[len(instance)-1]])))
 		# Normalize coordinates
-		for datum in self.DataCoordinates:
+		for datum in DataCoordinates:
 			datum[0] = datum[0] / MaxX
 			datum[1] = datum[1] / MaxY
-		return self.DataCoordinates
+		return DataCoordinates, Classes
 
-	def FindPoles(data):
+	def FindPoles(self,data):
 		this = randomelement(data)
 		data.remove(this)
 		East = farthestfrom(this,data)
@@ -50,9 +75,8 @@ class CompassGraph:
 	
 def main():
 	parser = OptionParser()
-	parser.add_option("--arff", dest="arff", default=None, metavar="FILE", 					help="An arff file to graph.")
-	parser.add_option("--gnuplot", dest="gnuplot", default=False, 					metavar="NONE", action="store_true", 
-				help="Use gnuplot to write graph to file")
+	parser.add_option("--arff", dest="arff", default=None, metavar="FILE", help="Make arff file to graph.")
+	parser.add_option("--gnuplot", dest="gnuplot", default=False, 	metavar="NONE", action="store_true", help="Use gnuplot to write graph to file")
 	(options, args) = parser.parse_args()
 	
 	if options.arff == None:
@@ -61,7 +85,7 @@ def main():
 
 	arff = Arff(options.arff)
 	compassgraph = CompassGraph(arff.data)
+	compassgraph.WriteToPNG(options.arff.split('.')[0])
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
 	main()
--- trunk/src/python/compass.py
+++ trunk/src/python/compass.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 #!/opt/local/bin/python
 from arff import *
 import csv
