0bdbdbe2c94b94ebdc653c6409ebbe74fb5abc4b
Andrew Butcher <abutcher@afrolegs.com>
2010-09-29 21:05:42 -0400

    Completed auc routine.

--- trunk/src/lisp/auc.lisp
+++ trunk/src/lisp/auc.lisp
@@ -2,18 +2,22 @@
 
 (defun auc-two-point (p1 p2)
   (let ((area-triangle
-	 (* 0.5
-	    (abs (- (point-y p1) (point-y p2)))
+	 (* (* 0.5 (abs (- (point-y p1) (point-y p2))))
 	    (- (point-x p2) (point-x p1))))
 	(area-square
-	 (* (point-y p1) (- (point-x p2) (point-x p1)))))
+	 (* (max (point-y p1) (point-y p2)) (- (point-x p2) (point-x p1)))))
     (- area-square area-triangle)))
 
 (defun auc (points)
   (let ((total-area 0))
-    (dotimes (n length points)
-      (unless (= n (length points))
+    (dotimes (n (length points))
+      (unless (= (1+ n) (length points))
 	(setf total-area (+ total-area
 			    (auc-two-point (nth n points) (nth (1+ n) points))))))
-    total-area))
+    (abs total-area)))
 
+(defun tuples-to-points (tuples)
+  (let (points)
+    (dolist (tuple tuples)
+      (push (make-point :x (first tuple) :y (second tuple)) points))
+    (reverse points)))
