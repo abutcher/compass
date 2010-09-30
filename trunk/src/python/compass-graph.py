bd9d2d1d06472692fd3c65d3962a69feafea8a2a
Kel Cecil <kelcecil@praisechaos.com>
2010-09-29 22:55:21 -0400

    updating compass-graph and adding datasets

--- trunk/src/python/compass-graph.py
+++ trunk/src/python/compass-graph.py
@@ -3,6 +3,7 @@
 from arff import *
 import csv
 import numpy
+import matplotlib.lines as lines
 import matplotlib.pyplot as plt
 from optparse import OptionParser
 from util import *
@@ -19,21 +20,50 @@ class CompassGraph:
 		(self.East, self.West) = self.FindPoles(InputData)
 		(self.DataCoordinates, self.Classes) = self.ComputeCoordinates()
 
-	def WriteToPNG(self,filename):
+	def WriteToPNG(self,filename,n):
 		self.DataCoordinates = self.DataCoordinates.transpose()
-		plt.axis([0,1,0,1])
+		fig = plt.figure()
+		ax1 = fig.add_axes([0.1,0.1,0.85,0.85])
+		ticks = []
+		for i in range(n):
+			ticks.append( ( float(i)+1.0 )* (1.0/float(n) ) )
+		ax1.set_xticks(ticks)
+		ax1.set_yticks(ticks)
 		plt.title(filename)
 		plt.xlabel("x")
 		plt.ylabel("y")
-		plt.grid(True)
+		for ax in fig.axes:
+			ax.grid(True)
 		if isinstance(self.Classes[0][0],str):
 			for i in range(len(self.DataCoordinates[0])):
 				if self.Classes[i][0].upper() == "TRUE":
-					plt.plot(self.DataCoordinates[0][i],self.DataCoordinates[1][i],"bo")
+					ax1.plot(self.DataCoordinates[0][i],self.DataCoordinates[1][i],"bo")
 				else:
-					plt.plot(self.DataCoordinates[0][i],self.DataCoordinates[1][i],"ro")
-		a = plt.gca()
+					ax1.plot(self.DataCoordinates[0][i],self.DataCoordinates[1][i],"ro")
+		else:
+			# we're going to color by highest and lowest.  Best of luck to me.
+			# begin by normalizing the effort scores so we get a fluent gradient
+			MaxScore=0
+			MinScore=999999999
+			NormalData=[]
+			# find our min and max scores.
+			for i in range(len(self.data)):
+				if self.data[i][len(self.data[i])-1] < MinScore:
+					MinScore = self.data[i][len(self.data[i])-1]
+				if self.data[i][len(self.data[i])-1] > MaxScore:
+					MaxScore = self.data[i][len(self.data[i])-1]
+			# set the normalized values
+			for i in range(len(self.data)):
+				NormalData.append((self.data[i][len(self.data[i])-1]-MinScore)/(MaxScore-MinScore))
+			# now, draw the data
+			for i in range(len(self.DataCoordinates[0])):
+				ax1.plot(self.DataCoordinates[0][i],self.DataCoordinates[1][i],'o',color=[1-NormalData[i],NormalData[i],0])
+			
+
+		ax1.set_xbound(0,1)
+		ax1.set_ybound(0,1)
 		plt.show()
+
 				
 
 	def ComputeCoordinates(self):
@@ -70,13 +100,14 @@ class CompassGraph:
 		data.remove(East)
 		data.append(this)
 		West = farthestfrom(East,data)
+		data.append(East)
 		return East, West
 	
 	
 def main():
 	parser = OptionParser()
 	parser.add_option("--arff", dest="arff", default=None, metavar="FILE", help="Make arff file to graph.")
-	parser.add_option("--gnuplot", dest="gnuplot", default=False, 	metavar="NONE", action="store_true", help="Use gnuplot to write graph to file")
+	parser.add_option("--n", dest="n", default=4, metavar="N", help="Set n to draw n x n grids.")
 	(options, args) = parser.parse_args()
 	
 	if options.arff == None:
@@ -85,7 +116,7 @@ def main():
 
 	arff = Arff(options.arff)
 	compassgraph = CompassGraph(arff.data)
-	compassgraph.WriteToPNG(options.arff.split('.')[0])
+	compassgraph.WriteToPNG(options.arff.split('.')[0],int(options.n))
 
 if __name__ == '__main__':
 	main()
