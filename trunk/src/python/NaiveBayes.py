72c09678cd937a432c7d29172af01e1fa328f06e
Kel Cecil <kelcecil@praisechaos.com>
2010-10-06 21:53:07 -0400

    debugging naive bayes and knn.  Looks like it works pretty well.

--- trunk/src/python/NaiveBayes.py
+++ trunk/src/python/NaiveBayes.py
@@ -1,3 +1,5 @@
+from math import *
+
 class Normal:
     largest = []
     smallest = []
@@ -7,43 +9,45 @@ class Normal:
     
     def __init__(self,data):
         for i in range(len(data)-1):
-            largest.append(0)
-            smallest.append(99999999)
-            n.append(len(data))
-            Sum.append(0)
-            SumSquared.append(0)
+            self.largest.append(0)
+            self.smallest.append(99999999)
+            self.n.append(len(data))
+            self.Sum.append(0)
+            self.SumSquared.append(0)
 
         for instance in data:
             for i in range(len(instance)-1):
-                if instance[i] > largest[i]:
-                    largest[i] = instance[i]
-                if instance[i] < smallest[i]:
-                    smallest[i] = instance[i]
-                Sum[i] = Sum[i] + instance[i]
-                SumSquared = SumSquared[i] + pow(instance[i],2)
+                if instance[i] > self.largest[i]:
+                    self.largest[i] = instance[i]
+                if instance[i] < self.smallest[i]:
+                    self.smallest[i] = instance[i]
+                self.Sum[i] = self.Sum[i] + instance[i]
+                self.SumSquared[i] = self.SumSquared[i] + pow(instance[i],2)
 
     def mean(self,Index):
         return self.Sum[Index] / self.n[Index]
 
     def stdev(self,Index):
-        return math.sqrt( (self.SumSquared - ( math.pow(self.Sum[Index],2) / self.n[Index] ))
-                   (self.n[Index] - 1))
+        try:
+            return sqrt( (self.SumSquared[Index] - ( pow(self.Sum[Index],2) / self.n[Index] )) / (self.n[Index] - 1))
+        except ValueError:
+            return 0.00000001
 
     def GaussianPDF(self,Index,x):
-        return ( 1 / math.sqrt(2 * math.pi * math.pow(self.stdev(self,Index),2) )) * math.pow(math.e,-1 * ( math.pow( x - self.stdev(Index),2) / (2 * math.pow(self.stdev(Index),2))))
+        return ( 1 / sqrt(2 * pi * pow(self.stdev(Index),2) )) * pow(e,-1 * ( pow( x - self.stdev(Index),2) / (2 * pow(self.stdev(Index),2))))
             
         
 
-def Naive-Bayes-Classify(instance, data, m=2, k=1):
+def NaiveBayesClassify(instance, data, m=2, k=1):
     data.remove(instance)
     Classification = ["blank", -100]
     (ClassList,ClassNormal) = GenerateNormalForClasses(data)
-    for ClassIndex in range(ClassList):
+    for ClassIndex in range(len(ClassList)):
         # Calculate the prior
-        tmp = math.log( ClassFrequency[ClassIndex] + k ) / ( len(data) + ( k * len(ListOfClasses)))
+        tmp = log( ClassNormal[ClassIndex].n[0] + k ) / ( len(data) + ( k * len(ClassList)))
         for FeatureIndex in range(len(instance)-1):
             # Gaussian PDF function.  May fix for Discrete values later.
-            tmp = tmp + math.log(ClassNormal[ClassIndex].GaussianPDF(FeatureIndex,instance[FeatureIndex]))
+            tmp = tmp + ClassNormal[ClassIndex].GaussianPDF(FeatureIndex,instance[FeatureIndex])
         if Classification[1] < tmp:
             Classification[0] = ClassList[ClassIndex]
             Classification[1] = tmp
@@ -60,7 +64,7 @@ def GenerateNormalForClasses(data):
             ClassInstances[ClassIndex.index(instance[len(instance)-1])].append(instance)
         except ValueError:
             ClassIndex.append(instance[len(instance)-1])
-            ClassInstances.append = [instance]
+            ClassInstances.append([instance])
     for ClassNorm in ClassInstances:
         ClassNormal.append(Normal(data))
     return ClassIndex, ClassNormal    
