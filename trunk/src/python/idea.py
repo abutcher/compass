7dec2b7ac44fcf0cccda2e03a66577101a882d36
Andrew Butcher <abutcher@afrolegs.com>
2010-10-17 14:00:08 -0400

    Working on the color ranges for performance scores.  This is what I have so far, a key would be amazing.

--- trunk/src/python/idea.py
+++ trunk/src/python/idea.py
@@ -6,6 +6,7 @@ import numpy
 import matplotlib.lines as lines
 import matplotlib.pyplot as plt
 import matplotlib.collections as collections
+from matplotlib import cm, colors
 from optparse import OptionParser
 from util import *
 from quadrant import *
@@ -55,8 +56,9 @@ class Idea:
                         yticks = EqualFrequencyTicks(self.DataCoordinates.transpose(),1,Parameters.n)
 
 
-		ax1.set_xticks(xticks)
-		ax1.set_yticks(yticks)
+		#ax1.set_xticks(xticks)
+		#ax1.set_yticks(yticks)
+
 		plt.title(filename)
 		plt.xlabel("x")
 		plt.ylabel("y")
@@ -87,48 +89,24 @@ class Idea:
 			self.DataCoordinates = self.DataCoordinates.transpose()
 			Quadrants = self.MakeQuadrants(Parameters, xticks, yticks)
 
+			# Display the cuts we made.  Since we don't care about unclustered regions, leaving this out.
 			"""
-			for i in range(len(Quadrants)):
-			if (Quadrants[i].Data != []) and (len(Quadrants[i].Data) > 4):
-					xmin = Quadrants[i].xmin
-					xmax = Quadrants[i].xmax
-					ymin = Quadrants[i].ymin
-					ymax = Quadrants[i].ymax
-					ax.broken_barh([ (xmin, (xmax - xmin)) ], (ymin, (ymax - ymin)) , facecolors='gold', alpha='0.5')
-
-				else:
-					xmin = Quadrants[i].xmin
-					xmax = Quadrants[i].xmax
-					ymin = Quadrants[i].ymin
-					ymax = Quadrants[i].ymax
-					ax.broken_barh([ (xmin, (xmax - xmin)) ], (ymin, (ymax - ymin)) , facecolors='gray', alpha='0.2')
-			"""
-
-			# Display the cuts we made
 			for i in range(len(Quadrants)):
 				xmin = Quadrants[i].xmin
 				xmax = Quadrants[i].xmax
 				ymin = Quadrants[i].ymin
 				ymax = Quadrants[i].ymax
 				ax.broken_barh([ (xmin, (xmax - xmin)) ], (ymin, (ymax - ymin)) , facecolors='white', linewidth=0.2)
-			
+			"""
+
 			# Color the clusters green.
 			Clusters = GRIDCLUS(Quadrants)
-			"""
-			for Cluster in Clusters:
-				for Quadrant in Cluster:
-					xmin = Quadrant.xmin
-					xmax = Quadrant.xmax
-					ymin = Quadrant.ymin
-					ymax = Quadrant.ymax
-					ax.broken_barh([ (xmin, (xmax - xmin)) ], (ymin, (ymax - ymin)) , facecolors='green', alpha='0.5')
-			"""
+
 			# Gather performance scores for each cluster
 			Scores = []
 			for Cluster in Clusters:
 				Scores.append((PerformanceScore(Cluster), Cluster))
 			
-			TYPE=""
 			if type(self.data[0][-1]) is str:
 				Scores = sorted(Scores, key=lambda score: score[0], reverse=True)
 				TYPE = "DEFECT"
@@ -142,30 +120,22 @@ class Idea:
 				else:
 					print "MDMRE: %.2f" % Scores[i][0]
 
-				# Top 25%
-				if (i < round(len(Scores)/4)):
-					for Quadrant in Scores[i][1]:
-						xmin = Quadrant.xmin
-						xmax = Quadrant.xmax
-						ymin = Quadrant.ymin
-						ymax = Quadrant.ymax
-						ax.broken_barh([ (xmin, (xmax - xmin)) ], (ymin, (ymax - ymin)) , facecolors='green', alpha='0.5', linewidth=0.2)
-				# Middle 50%
-				elif (i < round(len(Scores)/2)) and (i > round(len(Scores)/4)):
-					for Quadrant in Scores[i][1]:
-						xmin = Quadrant.xmin
-						xmax = Quadrant.xmax
-						ymin = Quadrant.ymin
-						ymax = Quadrant.ymax
-						ax.broken_barh([ (xmin, (xmax - xmin)) ], (ymin, (ymax - ymin)) , facecolors='yellow', alpha='0.5', linewidth=0.2)
-				# Bottom 25%
-				elif (i < len(Scores)) and (i > round(len(Scores)/2)):
-					for Quadrant in Scores[i][1]:
-						xmin = Quadrant.xmin
-						xmax = Quadrant.xmax
-						ymin = Quadrant.ymin
-						ymax = Quadrant.ymax
-						ax.broken_barh([ (xmin, (xmax - xmin)) ], (ymin, (ymax - ymin)) , facecolors='red', alpha='0.5', linewidth=0.2)
+					
+				def make_N_colors(cmap_name, N):
+					cmap = cm.get_cmap(cmap_name, N)
+					return cmap(np.arange(N))
+
+				def ColorQuadrants(quadrants, color):
+					for quadrant in quadrants:
+						xmin = quadrant.xmin
+						xmax = quadrant.xmax
+						ymin = quadrant.ymin
+						ymax = quadrant.ymax
+						ax.broken_barh([ (xmin, (xmax - xmin)) ], (ymin, (ymax - ymin)) , color=color, linewidth=0.2)
+
+				colors = make_N_colors(cm.hot, len(Scores)*10)
+				colors = colors[::-1]
+				ColorQuadrants(Scores[i][1], colors[i*10])
 				
 		return fig
 
@@ -245,8 +215,6 @@ class Idea:
 
 		minn = minx
 		maxn = maxy
-#		minn = yticks[1]
-#		maxn = xticks[1]
 
 		# Bottom Left
 		Quadrants.append(self.MakeQuadrant(minn, x, minn, y, self.DataCoordinates, self.data))
@@ -257,7 +225,7 @@ class Idea:
 		# Top Right
 		Quadrants.append(self.MakeQuadrant(x, maxn, y, maxn, self.DataCoordinates, self.data))
 
-		# Treat quadrants as a tree, where root is the initial board.
+		# Treat quadrants as a tree, where root is the initial block.
 		root = self.MakeQuadrant(minn,maxn,minn,maxn, self.DataCoordinates, self.data)
 		root.children = Quadrants
 
@@ -289,27 +257,6 @@ class Idea:
 		collectleaves(root)
 		Quadrants = leaflist
 
-		"""
-		# This is simply for --n integer and has not yet been implemented for equal freq etc.
-		# Assuming axes are 1.0 -> 1.0 and split by Parameters.n
-		for x in range(Parameters.n):
-			for y in range(Parameters.n):
-				xmin = float(x)/Parameters.n
-				xmax = (float(x)/Parameters.n) + (1.0/Parameters.n)
-				ymin = float(y)/Parameters.n
-				ymax = (float(y)/Parameters.n) + (1.0/Parameters.n)
-				print "Quadrant defined by\n\txmin: %f\n\txmax: %f\n\tymin: %f\n\tymax: %f\n" % (xmin, xmax, ymin, ymax)
-				data = []
-				for i in range(len(self.DataCoordinates)):
-					xcoord = self.DataCoordinates[i][0]
-					ycoord = self.DataCoordinates[i][1]
-					if ( xcoord >= xmin and xcoord <= xmax ) and ( ycoord >= ymin and ycoord <= ymax ):
-						data.append(Instance(self.DataCoordinates[i], self.data[i]))
-					Quadrants.append(Quadrant(xmin, xmax, ymin, ymax, data))
-				print "Has data:"
-				for instance in data:
-					print instance.datum
-		"""
 		return Quadrants
 
 	def MakeQuadrant(self, xmin, xmax, ymin, ymax, DataCoordinates, data):
