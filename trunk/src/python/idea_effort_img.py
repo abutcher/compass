0e0babb320ee7a6dc384c1c82c988da68266f3cf
Andrew Butcher <abutcher@afrolegs.com>
2011-02-04 17:25:42 -0500

    Adding an image rig for the effort experiments.

--- trunk/src/python/idea_effort_img.py
+++ trunk/src/python/idea_effort_img.py
@@ -12,39 +12,37 @@ from util import *
 from copy import deepcopy
 from bore import *
 from NaiveBayes import *
+import random
 
 def main():
+    random.seed(5)
     args = parse_options()
 
     title = args.train[0].split("/")[-1].split(".")[0]
     arff = Arff(args.train[0])
-
-    print "dataset,%s" % (title)
+    
+    print "Running dataset %s, outputting to %s.png" % (title, title)
 
     dc = DataCollection(arff.data)
     ic = InstanceCollection(dc)
     ic.normalize_coordinates()
 
-    stats = EffortStats()
-    
-    for i in range(len(ic.instances)):
-        test = deepcopy(ic.instances[i])
-        train = deepcopy(ic.instances)
-        train.remove(test)
+    train = deepcopy(ic.instances)
+    trainXY = log_y(log_x(deepcopy(train)))
 
-        trainXY = log_y(log_x(deepcopy(train)))
-        # Log functions need to check for just one item.
-        testXY = log_y(log_x(deepcopy(test)))
+    print "Generating quadrant tree..."
+    qtree = QuadrantTree(trainXY)
 
-        qtree = QuadrantTree(trainXY)
+    print "Culling high variance branches..."
+    # Implement method for culling high variance quadrants.
 
-        # Implement method for culling high variance quadrants.
-        # Traverse quadrant tree to find a suitable quadrant.
-        # got = ...
-
-        stats.Evaluate(got, testXY[-1])
-
-    print "mdmre, %.2f" % (stats.MDMRE())
+    print "Creating image from regions of good/badness..."
+    # Pass of quadrants to Figure to generate good and bad regions
+    # based on variance.
+        
+    print "Writing figure %s.png ..." % (title)
+    fig = Figure(title, trainXY, quadrants, culled_quadrants)
+    fig.write_png()
 
 def parse_options():
     """Place new options that you would like to be parsed here."""
@@ -55,16 +53,8 @@ def parse_options():
                         type=str,
                         nargs='+',
                         help='Specify arff file[s] from which to construct the training set.')
-    parser.add_argument('--accept',
-                        dest='accept',
-                        default=None,
-                        metavar='FLOAT 0-1',
-                        type=float,
-                        nargs=1,
-                        help='Specify the acceptance rate to use when generating clusters.')
     args = parser.parse_args()
     return args
 
 if __name__ == "__main__":
     main()
-
